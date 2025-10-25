fd596055634bb094ff0af4862f32d481af005c02
defan6 <valayevfedya2005@gmail.com>
2025-10-25 16:13:16 +0300

    app is ready

--- src/main/java/com/yourcompany/hellok8s/HelloController.java
+++ src/main/java/com/yourcompany/hellok8s/HelloController.java
@@ -3,12 +3,13 @@ package com.yourcompany.hellok8s;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
 
-    @GetMapping
+    @GetMapping("/hello")
     public String helloController(){
         return "Hello from k8s!!";
     }
